136d90366f80bf60d170da8df492a3706197e51e
Anthony Lewandowski <lewandowski.anthony.pro@gmail.com>
2021-03-06 17:39:03 +0100

    Ajout du code

---
+++ jsonHandler/jsonHandler.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Security;
+
+namespace jsonManipulation
+{
+    public class jsonHandler
+    {
+        private String jsonPath;
+        private String usedJson;
+
+        public jsonHandler()
+        {
+            this.jsonPath = "";
+        }
+
+        public jsonHandler(string jsonPath)
+        {
+            this.jsonPath = jsonPath;
+        }
+
+        public String readJsonFile()
+        {
+            using (var reader = new StreamReader(jsonPath))
+            {
+                this.usedJson = reader.ReadToEnd();
+                return this.usedJson;
+            }
+        }
+
+        public String replaceAttributeNameInJson(String wordToReplace, String remplacementWord)
+        {
+            String replacement = usedJson.Replace("\"" + wordToReplace + "\"" + " : ", "\"" + remplacementWord + "\"" + " : ");
+            replacement = replacement.Replace(wordToReplace + ":", remplacementWord + ":");
+            usedJson = replacement;
+            return usedJson;
+        }
+
+        public void suppressAttributeInJson()
+        {
+
+        }
+
+        public void exportJson(String _filePath, String _fileName)
+        {
+            String fullPath = @"" + _filePath + "\\" + _fileName + ".json";
+            File.Create(fullPath).Close();
+            try
+                {
+                    File.WriteAllText(fullPath, usedJson);
+                }
+                catch (SecurityException ex)
+                {
+                    //present dialog
+                }
+                catch (Exception ex)
+                {
+                    //All other exceptions handled the same
+                }
+        }
+
+        public string getJsonPath { get { return jsonPath; } set {;} }
+    }
+}
---
+++ testApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using jsonManipulation;
+
+namespace testApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            String _pathJson = "C:\\Users\\lewan\\source\\repos\\jsonHandler\\testApp\\test.json";
+            jsonHandler myHandler = new jsonHandler(_pathJson);
+            Console.WriteLine(myHandler.getJsonPath);
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(myHandler.readJsonFile());
+            Console.WriteLine("------------------------------------");
+            Console.WriteLine(myHandler.replaceAttributeNameInJson("test", "ValeurDeTest"));
+            myHandler.exportJson("C:\\Users\\lewan\\source\\repos\\jsonHandler\\testApp", "returned");
+        }
+    }
+}
